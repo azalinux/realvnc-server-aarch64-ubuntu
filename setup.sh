880364e9560efecced39ad1e271485c4167595f0
Azalinux <52433182+azalinux@users.noreply.github.com>
2022-09-30 11:56:33 +1000

    version bump to 6.11.0

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ sudo ln -s /usr/lib/aarch64-linux-gnu/libbcm_host.so /usr/lib/libbcm_host.so.0
 sudo ln -s /usr/lib/aarch64-linux-gnu/libvchiq_arm.so /usr/lib/libvchiq_arm.so.0
 sudo ln -s /usr/lib/aarch64-linux-gnu/libvcos.so /usr/lib/libvcos.so.0
 
-sudo dpkg -i VNC-Server-6.10.1-Linux-ARM64.deb
+sudo dpkg -i VNC-Server-6.11.0-Linux-ARM64.deb
 
 sudo systemctl enable --now vncserver-x11-serviced 
